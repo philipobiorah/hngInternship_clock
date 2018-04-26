aac9a0d705921dac779ef91781696dfe6803c982
Philip Obiorah <philip.obiorah@uniport.edu.ng>
2018-04-26 23:13:16 +0100

    added comments

--- index.php
+++ index.php
@@ -9,41 +9,37 @@
     <link href="https://fonts.googleapis.com/css?family=Audiowide|Jura|Marvel|Orbitron" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Astloch|Iceland|Wallpoet" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet" type="text/css"/>
-    <?php
-date_default_timezone_set('UTC');
-?>
-<script>
-var d = new Date(<?php echo time() * 1000 ?>);
-function digitalClock() {
-  d.setTime(d.getTime() + 1000);
-  var hrs = d.getHours();
-  var mins = d.getMinutes();
-  var secs = d.getSeconds();
-  mins = (mins < 10 ? "0" : "") + mins;
-  secs = (secs < 10 ? "0" : "") + secs;
-  var apm = (hrs < 12) ? "am" : "pm";
-  hrs = (hrs > 12) ? hrs - 12 : hrs;
-  hrs = (hrs == 0) ? 12 : hrs;
-  var ctime = hrs + ":" + mins + ":" + secs + " " + apm;
-  document.getElementById("clock").firstChild.nodeValue = ctime;
-}
-window.onload = function() {
-  digitalClock();
-  setInterval('digitalClock()', 1000);
-}
-</script>
+   
+    <?php date_default_timezone_set('UTC');?>
+    <script>
+    var d = new Date(<?php echo time() * 1000 ?>);     // gets the current time using a Php echo time function
+    function digitalClock() {
+      d.setTime(d.getTime() + 1000);
+      var hrs = d.getHours();
+      var mins = d.getMinutes();
+      var secs = d.getSeconds();
+      mins = (mins < 10 ? "0" : "") + mins;
+      secs = (secs < 10 ? "0" : "") + secs;
+      var apm = (hrs < 12) ? "am" : "pm";
+      hrs = (hrs > 12) ? hrs - 12 : hrs;
+      hrs = (hrs == 0) ? 12 : hrs;
+      var ctime = hrs + ":" + mins + ":" + secs + " " + apm;
+      document.getElementById("clock").firstChild.nodeValue = ctime;
+    }
+    window.onload = function() {
+      digitalClock();
+      setInterval('digitalClock()', 1000);
+    }
+    </script>
 
 
 </head>
 <body>
     
-<div class="heading">Hotels.ng</div>
-<div class="sub_head">Internship</div>
+  <div class="heading">Hotels.ng</div>
+  <div class="sub_head">Internship</div>
   <div><p>&lt;letsCode/&gt;</p></div>
-<div id="clock" class="time">Time</div>
+  <div id="clock" class="time">Time</div>
 
-
-
-             
 </body>
 </html>
